60cca4cb547afd9ecc543990378e1e82950f190e
Jorge Mario Fernandez <jmferna91@gmail.com>
2020-03-05 07:52:50 -0500

    contact and index netlify update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, FormControl, Validators } from "@angular/forms";
 import { MessageService } from '../message.service';
-
+import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
 
 // import { MatFormFieldModule } from '@angular/material/form-field'; 
 // import { MatButtonModule } from '@angular/material/form-field'; 
@@ -21,6 +21,13 @@ export class ContactComponent implements OnInit {
     submitted = false;
     success = false;
 
+    contactForm = new FormGroup({
+        name: new FormControl(''),
+        email: new FormControl(''),
+        art: new FormControl(''),
+        message: new FormControl(''),
+    });
+
     /// adding items to arguments is called 'dependency injection'
     // constructor(private formBuilder: FormBuilder) {
     //     this.messageForm = this.formBuilder.group({
@@ -29,30 +36,63 @@ export class ContactComponent implements OnInit {
     //     })
     // }
 
-    constructor(private messageService: MessageService) {}
+    //constructor(private messageService: MessageService) {}
+    constructor(
+        private http: HttpClient
+      ) {};
 
-    send() {
-        console.log("message sent!!")
+      onSubmit() {
+        const body = new HttpParams()
+        .set('form-name', 'contact')
+        .append('name', this.contactForm.value.name)
+        .append('email', this.contactForm.value.email)
+        .append('art', this.contactForm.value.art)
+        .append('message', this.contactForm.value.message)
+        this.http.post('/', body.toString(), {headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}).subscribe(
+          res => {},
+          err => {
+            if (err instanceof ErrorEvent) {
+              //client side error
+              alert("Something went wrong when sending your message.");
+              console.log(err.error.message);
+            } else {
+              //backend error. If status is 200, then the message successfully sent
+              if (err.status === 200) {
+                alert("Your message has been sent!");
+              } else {
+                alert("Something went wrong when sending your message.");
+                console.log('Error status:');
+                console.log(err.status);
+                console.log('Error body:');
+                console.log(err.error);
+              };
+            };
+          }
+        );
+      };
+
+    // send() {
+    //     console.log("message sent!!")
        
-        this.messageService.sendMessagePost("1234");
-        this.messageService.sendMessageGet("4321");
+    //     this.messageService.sendMessagePost("1234");
+    //     this.messageService.sendMessageGet("4321");
        
-    }
+    // }
 
-    onSubmit() {
-        this.submitted = true;
-        alert("clicked!")
-        //console.log("message sent");
+    // onSubmit() {
+    //     this.submitted = true;
+    //     alert("clicked!")
+    //     //console.log("message sent");
 
-        if(this.messageForm.invalid){
-            return;
-        }
+    //     if(this.messageForm.invalid){
+    //         return;
+    //     }
 
-        this.success = true;
-       // this.messageForm;
-        console.log("message sent");
+    //     this.success = true;
+    //    // this.messageForm;
+    //     console.log("message sent");
         
-    }
+    // }
 
     ngOnInit(): void {
         
